19eef5325c68fd1a58b338f4e5518be8786cab7d
ammarasid <ammara.siddiqui7@gmail.com>
2025-11-09 08:56:22 -0600

    Add files via upload

---
+++ uptimehealth.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    console.log('Axios Test Response:', response.data); 
+  })
+  .catch(error => {
+    console.error('Error with Axios test:', error);  
+  });
+
+// Your UptimeRobot API Key (replace with your actual key)
+const apiKey = '';  
+
+const url = 'https://api.uptimerobot.com/v2/getMonitors';
+
+const checkUptime = async () => {
+  try {
+    
+    const data = {
+      api_key: apiKey,
+      format: 'json',
+    };
+
+    
+    const response = await axios.post(url, data);
+    
+    
+    console.log('Full Response:', response.data);
+
+    
+    if (response.status === 200) {
+      const monitors = response.data.monitors;
+      if (monitors && monitors.length > 0) {
+        monitors.forEach(monitor => {
+          console.log(`Monitor Name: ${monitor.friendly_name}, Status: ${monitor.status}`);
+        });
+      } else {
+        console.log('No monitors found or response format is unexpected');
+      }
+    } else {
+      console.log(`Failed to fetch data: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+
+checkUptime();
